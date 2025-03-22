f40c3958ede6d4deba72e4480f25de2e63bd5e10
dishan1223 <dishanishtiaq45+github@gmail.com>
2025-03-22 08:57:59 +0600

    superman

--- src/components/ui/MaxWidthContainer.tsx
+++ src/components/ui/MaxWidthContainer.tsx
@@ -4,6 +4,6 @@ interface MaxWidthContainerProps {
 // className : max-w-7xl mx-auto px-4 sm:px-6 lg:px-8
 export default function MaxWidthContainer({ children }: MaxWidthContainerProps) {
     return (
-        <div className="">{children}</div>
+        <div className="mx-auto px-4 sm:px-6 lg:px-8 py-8">{children}</div>
     );
 }
